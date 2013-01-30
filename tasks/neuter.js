5dfe222d6453f0bee655adea3f5961178bb9bb4c
Chad Hietala <chadhietala@gmail.com>
2013-01-30 13:04:59 -0500

    Fix issues with templates

--- tasks/neuter.js
+++ tasks/neuter.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
     }
     
     var options = this.options({
-      template: "(function() {\n\n<%= src %>\n\n})();",
+      template: "(function() {\\\\n\\\\n<%= src %>\\\\n\\\\n})();",
       separator: "\n\n",
       includeSourceURL: false,
       baseURL: '',
